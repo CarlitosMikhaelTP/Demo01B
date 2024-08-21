0a1ecc915f937b0ebdef38efe096241f6d3b96ed
Alumno <Alumno@LAB1507-11>
2024-08-20 19:51:13 -0500

    Agregar archivos de proyecto.

---
+++ Demo01B/Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01B
+{
+    public class Persona
+    {
+        public Persona() {
+        }
+        public Persona(string nombres, string apellidos) 
+        {
+
+        }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+
+
+       
+
+    }
+}
---
+++ Demo01B/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Carlitos";
+            persona.Apellido = "Torres";
+
+            Persona persona2 = new Persona("Mikhael", "Pérez");
+
+            Persona persona3 = new Persona
+            {
+                Nombre = "Michael",
+                Apellido = "Salazar",
+                Edad = 2
+            };
+            //persona.AsignarNombresCompletos();
+            Triangulo triangulo = new Triangulo();
+            triangulo.Base = 7;
+            triangulo.Altura = 8;
+            
+
+            //Console.WriteLine(persona.NombresCompletos);
+            Console.WriteLine(triangulo.CalcularArea());
+            Console.Read();
+        }
+    }
+}
